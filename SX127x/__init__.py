166dcc1b566c54f8f8dd5a3c9a42e47abe406c54
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-11-19 10:18:40 -0500

    Updated imports

--- SX127x/__init__.py
+++ SX127x/__init__.py
@@ -1,1 +1,3 @@
-__all__ = ['SX127x']
+from SX127x.LoRa import *
+from SX127x.LoRaArgumentParser import *
+from board_config_ada import *
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'SX127x',         # How you named your package folder (MyLib)
   packages = ['SX127x'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = '',   # Give a short description about your library
   author = 'Colby Sawyer',                   # Type in your name
   author_email = 'sawyerco21@ecu.edu',      # Type in your E-Mail
   url = 'https://github.com/ColbySawyer7/SX127x.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ColbySawyer7/SX127x/archive/refs/tags/v_0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ColbySawyer7/SX127x/archive/refs/tags/v_0.3.tar.gz',    # I explain this later on
   keywords = ['LoRaWAN', 'Lora', 'Things Network', 'TTN', 'v3'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'RPi.GPIO',
